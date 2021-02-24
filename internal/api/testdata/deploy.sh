2873457cf1d0718ddd8ddeeea422f0ab9c2b1d33
AJ ONeal <coolaj86@gmail.com>
2021-02-23 19:34:33 -0700

    set and get job reports

---
+++ internal/api/testdata/deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+set -u
+#set -x
+
+echo "[${GIT_REPO_ID:-}#${GIT_REF_NAME:-}] Started at ${GIT_DEPLOY_TIMESTAMP:-}"
+sleep ${GIT_DEPLOY_TEST_WAIT:-0.1}
+echo "[${GIT_REPO_ID:-}#${GIT_REF_NAME:-}] Finished"
+
+echo "Reporting to ${GIT_DEPLOY_CALLBACK_URL} ..."
+curl -fsSL "${GIT_DEPLOY_CALLBACK_URL}" \
+    -H 'Content-Type: application/json' \
+    -d '
+    { "report":
+        { "name": "sleep test",
+          "status": "PASS",
+          "message": "a top level result group",
+          "results": [
+              { "name": "sub test", "status": "PASS", "message": "a sub group", "detail": "logs or smth" }
+          ]
+        }
+    }
+'
+
+echo "[${GIT_REPO_ID:-}#${GIT_REF_NAME:-}] Generated Report"
--- internal/jobs/testdata/deploy.sh
+++ internal/jobs/testdata/deploy.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 set -e
 set -u
-set -x
+#set -x
 
 echo "[${GIT_REPO_ID:-}#${GIT_REF_NAME:-}] Started at ${GIT_DEPLOY_TIMESTAMP:-}"
 sleep ${GIT_DEPLOY_TEST_WAIT:-0.1}
 echo "[${GIT_REPO_ID:-}#${GIT_REF_NAME:-}] Finished"
-
-# TODO start/end? duration?
-#curl -X POST "${GIT_DEPLOY_CALLBACK_URL}" -d '
-#    { "report": [ { "name":"sleep", "code":"PASS", "message":"", "details":"" } ] }
-#'
